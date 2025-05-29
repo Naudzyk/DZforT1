c21092c3153883106d4364b2210370a96a252c4a
Naudzyk <zhenya_nikolaev_1995@list.ru>
2025-05-29 19:18:38 +0800

    fix

--- src/main/java/com/example/DZforT1/service/Impl/ClientServiceImpl.java
+++ src/main/java/com/example/DZforT1/service/Impl/ClientServiceImpl.java
@@ -3,16 +3,13 @@ package com.example.DZforT1.service.Impl;
 import com.example.DZforT1.DTO.AccountResponseDTO;
 import com.example.DZforT1.DTO.ClientCreateDTO;
 import com.example.DZforT1.DTO.ClientResponseDTO;
-import com.example.DZforT1.aop.CachedAOP.Cached;
-import com.example.DZforT1.aop.LogDataSourceAOP.LogDataSourceError;
-import com.example.DZforT1.aop.MetricAOP.Metric;
+import com.example.DZforT1.aop.LogDataSourceError;
 import com.example.DZforT1.models.Account;
 import com.example.DZforT1.models.Client;
 import com.example.DZforT1.repository.ClientRepository;
 import com.example.DZforT1.service.ClientService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.hibernate.mapping.Collection;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -63,8 +60,6 @@ public class ClientServiceImpl implements ClientService {
     @Override
     @Transactional
     @LogDataSourceError
-    @Metric
-    @Cached
     public ClientResponseDTO getClientById(Long id) {
         Client client = clientRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Client not found"));
@@ -73,7 +68,6 @@ public class ClientServiceImpl implements ClientService {
 
     @Override
     @Transactional
-    @Metric
     public List<ClientResponseDTO> getAllClients() {
         return clientRepository.findAll().stream()
                 .map(this::convertToClientResponseDTO)
